33fa4f68712575e4fb3f95b7feb82d93d7f02348
Julio Ucharima <julio_ucharima@usmp.pe>
2023-10-22 12:42:05 -0500

    updated routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { HostalPage } from '../hostal/pages/HostalPage';
-import { PersonalPage } from '../staff/pages/PersonalPage';
+import { StaffPage } from '../staff/pages/StaffPage';
 
 export const AppRouter: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<HostalPage />} />
-      <Route path="/personal" element={<PersonalPage />} />
+      <Route path="/personal" element={<StaffPage />} />
     </Routes>
   );
 };
