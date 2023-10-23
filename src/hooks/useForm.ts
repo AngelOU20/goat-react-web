72fd10fae54513593c41d54a06f80f61a3d90711
Julio Ucharima <julio_ucharima@usmp.pe>
2023-10-23 16:34:38 -0500

    included custom Hook useForm

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,33 @@
+import { useState, type ChangeEvent } from 'react';
+
+// Interfaz para la estructura de retorno
+interface FormState<T> {
+  formState: T;
+  setFormState: React.Dispatch<React.SetStateAction<T>>;
+  onInputChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onResetForm: () => void;
+}
+
+export function useForm<T>(initialForm: T): FormState<T> {
+  const [formState, setFormState] = useState<T>(initialForm);
+
+  const onInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const onResetForm = (): void => {
+    setFormState(initialForm);
+  };
+
+  return {
+    formState,
+    setFormState,
+
+    onInputChange,
+    onResetForm,
+  };
+}
