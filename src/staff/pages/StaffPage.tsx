06ecc1e7a60ae3c00cf15427ba780dbe994af37b
Julio Ucharima <julio_ucharima@usmp.pe>
2023-10-24 22:04:00 -0500

    updated StaffPage

--- src/staff/pages/StaffPage.tsx
+++ src/staff/pages/StaffPage.tsx
@@ -1,24 +1,22 @@
 import { StaffTable, StaffModal } from '../components';
 import { HostalLayout } from '../../hostal/layout/HostalLayout';
-import { Container, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 
 export const StaffPage: React.FC = () => {
   return (
     <HostalLayout>
-      <Container>
-        <Typography
-          variant="h2"
-          sx={{
-            fontSize: '30px',
-            fontWeight: '700',
-            marginBottom: '4rem',
-          }}
-        >
-          Administrar Personal
-        </Typography>
-        <StaffTable />
-        <StaffModal />
-      </Container>
+      <Typography
+        variant="h2"
+        sx={{
+          fontSize: '30px',
+          fontWeight: '700',
+          marginBottom: '4rem',
+        }}
+      >
+        Administrar Personal
+      </Typography>
+      <StaffTable />
+      <StaffModal />
     </HostalLayout>
   );
 };
