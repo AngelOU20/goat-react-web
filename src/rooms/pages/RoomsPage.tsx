d862759e5c9f32e2b886fdc6e8fc15338eb07c71
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-08 22:23:52 -0500

    add RoomPage

---
+++ src/rooms/pages/RoomsPage.tsx
@@ -0,0 +1,24 @@
+import { Typography } from '@mui/material';
+import { HostalLayout } from '../../hostal/layout/HostalLayout';
+import { RoomsTable, TasksModal } from '../components';
+import { AssignStaffModal } from '../components/AssignStaffModal';
+
+export const RoomsPage: React.FC = () => {
+  return (
+    <HostalLayout>
+      <Typography
+        variant="h2"
+        sx={{
+          fontSize: '30px',
+          fontWeight: '700',
+          marginBottom: '4rem',
+        }}
+      >
+        Habitaciones
+      </Typography>
+      <RoomsTable />
+      <TasksModal />
+      <AssignStaffModal />
+    </HostalLayout>
+  );
+};
