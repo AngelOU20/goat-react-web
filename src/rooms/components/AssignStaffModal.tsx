40fb0bed266c52d5212c89daac8539cad31d05ba
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-22 21:33:18 -0500

    feat: add AssignStaffModal component

---
+++ src/rooms/components/AssignStaffModal.tsx
@@ -0,0 +1,106 @@
+import {
+  Backdrop,
+  Box,
+  Button,
+  Modal,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import { useUiRoomStore } from '../../hooks/useUiRoomStore';
+import { useStaffStore } from '../../staff/hooks';
+
+const style = {
+  position: 'relative',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 570,
+  height: 680,
+  bgcolor: 'background.paper',
+  borderRadius: '20px',
+  boxShadow: 24,
+  p: 5,
+};
+
+export const AssignStaffModal: React.FC = () => {
+  const { isAssignPersonalModalOpen, closeAssignPersonalModal } = useUiRoomStore();
+
+  const { staff } = useStaffStore();
+
+  const handleClose = (): void => {
+    closeAssignPersonalModal();
+  };
+
+  return (
+    <Modal
+      open={isAssignPersonalModalOpen}
+      onClose={handleClose}
+      closeAfterTransition
+      slots={{ backdrop: Backdrop }}
+      slotProps={{
+        backdrop: {
+          timeout: 100,
+        },
+      }}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="flex-start"
+        gap="20px"
+        sx={style}
+      >
+        <h2>Asignar Personal</h2>
+
+        <TableContainer>
+          <Table sx={{ minWidth: 450 }} size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ fontWeight: 700 }}>Nombre completo</TableCell>
+                <TableCell sx={{ fontWeight: 700 }} align="center">
+                  Horas Trabajadas
+                </TableCell>
+                <TableCell sx={{ fontWeight: 700 }} align="center">
+                  Tiempo de ejecución
+                </TableCell>
+                <TableCell sx={{ fontWeight: 700 }} align="center">
+                  Calidad
+                </TableCell>
+                <TableCell sx={{ fontWeight: 700 }} align="center"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {staff.map((personal) => (
+                <TableRow
+                  key={personal.nameComplete}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {personal.nameComplete}
+                  </TableCell>
+                  <TableCell align="center">8 horas</TableCell>
+                  <TableCell align="center">Medio</TableCell>
+                  <TableCell align="center">Regular</TableCell>
+                  <TableCell align="center">
+                    <Button
+                      variant="outlined"
+                      size="small"
+                      sx={{ textTransform: 'capitalize' }}
+                    >
+                      Asignar
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Modal>
+  );
+};
