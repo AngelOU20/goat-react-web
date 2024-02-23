6e0a619bd8a169ed6f56651795d29335cab9de3d
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-22 21:36:39 -0500

    refactor: update routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,14 +1,36 @@
-import { Route, Routes } from 'react-router-dom';
-import { HostalPage } from '../hostal/pages/HostalPage';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AuthRoutes } from '../auth/routes/AuthRoutes';
+import { Checking } from '../components/Checking';
+import { useAuthStore } from '../hooks/useAuthStore';
+import { DashboardPage } from '../dashboard/pages/DashboardPage';
 import { StaffPage } from '../staff/pages/StaffPage';
 import { RoomsPage } from '../rooms/pages/RoomsPage';
 
 export const AppRouter: React.FC = () => {
+  const { status } = useAuthStore();
+
+  if (status === 'checking') {
+    return <Checking />;
+  }
+
   return (
     <Routes>
-      <Route path="/" element={<HostalPage />} />
-      <Route path="/personal" element={<StaffPage />} />
-      <Route path="/habitaciones" element={<RoomsPage />} />
+      {status === 'not-authenticated' ? (
+        <>
+          <Route path="/auth/*" element={<AuthRoutes />} />
+          <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/personal" element={<StaffPage />} />
+          <Route path="/habitaciones" element={<RoomsPage />} />
+
+          <Route path="/*" element={<Navigate to="/" />} />
+        </>
+      )}
+
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   );
 };
