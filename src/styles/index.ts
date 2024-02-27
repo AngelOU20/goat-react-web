102d161461694538757aa83701c80ae328fcf9f2
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-27 14:55:14 -0500

    feat: add modal styles to styles file

---
+++ src/styles/index.ts
@@ -0,0 +1,25 @@
+export const modalStyle = {
+  position: 'relative',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 480,
+  height: 680,
+  bgcolor: 'background.paper',
+  borderRadius: '20px',
+  boxShadow: 24,
+  p: 5,
+};
+
+export const modalStyleAssignStaff = {
+  position: 'relative',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 570,
+  height: 680,
+  bgcolor: 'background.paper',
+  borderRadius: '20px',
+  boxShadow: 24,
+  p: 5,
+};
