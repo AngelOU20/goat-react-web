5be5991cb69290d5c51bd30b9a5356db63eec53b
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-28 15:39:57 -0500

    feat: implement custom hook useAuthStore

---
+++ src/hooks/useAuthStore.ts
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { type RootState } from '@/store';
+import { onChecking, onLogin, onLogout } from '@/store/auth/authSlice';
+
+interface AuthStoreOptions {
+  status: 'checking' | 'not-authenticated' | 'authenticated';
+  startLogin: () => void;
+  startLogout: () => void;
+}
+
+export const useAuthStore = (): AuthStoreOptions => {
+  const { status } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
+
+  const startLogin = (): void => {
+    dispatch(onChecking());
+
+    dispatch(onLogin());
+  };
+
+  const startLogout = (): void => {
+    dispatch(onLogout());
+  };
+
+  return {
+    status,
+    startLogin,
+    startLogout,
+  };
+};
