f746d0a6806e27e2677a0d350ee7ecd20a6ab71f
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-28 18:42:20 -0500

    refactor: update imports

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,10 +1,10 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
-import { Checking } from '../components/Checking';
+import { Checking } from '../app/components/Checking';
 import { useAuthStore } from '../hooks/useAuthStore';
-import { DashboardPage } from '../dashboard/pages/DashboardPage';
-import { StaffPage } from '../staff/pages/StaffPage';
-import { RoomsPage } from '../rooms/pages/RoomsPage';
+import { DashboardPage } from '../app/dashboard/pages/DashboardPage';
+import { StaffPage } from '../app/staff/pages/StaffPage';
+import { RoomsPage } from '../app/rooms/pages/RoomsPage';
 
 export const AppRouter: React.FC = () => {
   const { status } = useAuthStore();
